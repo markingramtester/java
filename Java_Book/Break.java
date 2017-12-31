e921a79107cd8c8f89e82be3d6850598f6f94bdc
Mark Ingram <34866382+markingramtester@users.noreply.github.com>
2017-12-31 13:21:40 +0000

    Example code for a Break and Continue in a for loop

--- Java_Book/Break.java
+++ Java_Book/Break.java
@@ -1,6 +1,6 @@
 class Break
 {
-	public static void main ( String[] args )
+	public static void main ( String[] args ) //Example code for a Break and Continue in a for loop
 	{
 		for (int i = 1 ; i < 4 ; i++)
 		{
@@ -14,7 +14,7 @@ class Break
 				if ( i == 2 && j == 1)
 				{
 					System.out.println("Breaks innerLoop when i="+i+" j="+j) ;
-					break ;
+					break ; //when i = 1 and j also = 1 this if stement will stop
 				}
 				System.out.println( "Running i="+i+" j="+j );
 			}
