0ce1089b8f53253229285ca7e6fd579dbfbaca25
Mark Ingram <34866382+markingramtester@users.noreply.github.com>
2017-12-31 12:45:17 +0000

    Example code that demonstrates order in which sums will be carried out

--- Java_Book/Precedence.java
+++ Java_Book/Precedence.java
@@ -1,9 +1,9 @@
 class Precedence
 {
-	public static void main ( String[] args )
+	public static void main ( String[] args ) //program that demonstrates order in which sums will be carried out
 	{
 		int sum = 32 - 8 + 16 * 2 ; //16 X 2 = 32, + 24 = 56
-		System.out.println( "Default order: " + sum) ;
+		System.out.println( "Default order: " + sum) ; 
 		
 		sum = (32 - 8 + 16) * 2 ; //24 + 16 = 40, X 2 = 80
 		System.out.println( "Specified order: " + sum) ;
