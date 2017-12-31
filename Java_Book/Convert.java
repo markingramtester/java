4ae99884c7128305f5051ded5487daa2c885aa06
Mark Ingram <34866382+markingramtester@users.noreply.github.com>
2017-12-31 13:12:29 +0000

    Example code for converting variable types

--- Java_Book/Convert.java
+++ Java_Book/Convert.java
@@ -1,11 +1,11 @@
 class Convert
 {
-	public static void main( String[] args )
+	public static void main( String[] args ) //Example code for converting variable types
 	{
 		float daysFloat = 365.25f ;
 		String weeksString = "52" ;
 		
-		int daysInt = (int) daysFloat ;
+		int daysInt = (int) daysFloat ; //changes floating number to integer
 		int weeksInt = Integer.parseInt(weeksString) ;
 		
 		int week = (daysInt / weeksInt ) ;
