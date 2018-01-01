542245a2767964551ab3ed90aa28144556d37291
Mark <markingramtester@gmail.com>
2018-01-01 17:28:04 +0000

    Example code demonstrates the use of a sub method

---
+++ Java_Book/Methods.java
@@ -0,0 +1,12 @@
+class Methods
+{
+	public static void main( String[] args ) //Example code demonstrates the use of a sub method
+	{
+		System.out.println("Message from the main method.") ;
+		sub() ; //Calls method called sub
+	}
+	public static void sub() //method called sub
+	{
+		System.out.println("Message from the sub method.") ;
+	}
+}
