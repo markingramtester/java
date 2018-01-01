f48d8ac1ba76decb844f8ad90eb0c35b6f4b70d9
Mark <markingramtester@gmail.com>
2018-01-01 11:17:57 +0000

    Example code demonstrating comparing values

---
+++ Java_Book/Compare.java
@@ -0,0 +1,11 @@
+class Compare
+{
+	public static void main( String[] args ) //Example code demonstrating comparing values
+	{
+		float num1 = 24.75f ;
+		int num2 = 25 ;
+		
+		System.out.println("Most is "+ Math.max(num1, num2)) ; //Returns highest number from stated variables
+		System.out.println("Least is "+ Math.min(num1, num2)) ; //Returns lowest number from stated variables
+	}
+}
