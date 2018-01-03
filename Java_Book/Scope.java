c9e90dc498ef7811d1b156eb18de34ba1eb3ef4a
Mark <markingramtester@gmail.com>
2018-01-03 19:31:15 +0000

    Example code demonstrating same named variables in different methods

---
+++ Java_Book/Scope.java
@@ -0,0 +1,14 @@
+class Scope
+{
+	public static void main( String[] args ) //Example code demonstrating same named variables in different methods
+	{
+		String txt = "This is a local variable in the main method" ;
+		System.out.println(txt) ;
+	sub() ;
+	}
+	public static void sub()
+	{
+		String txt = "This is a local variable in the sub method" ;
+		System.out.println(txt) ;
+	}
+}
