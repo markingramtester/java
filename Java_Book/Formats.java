27fb732bc4a73861e4e008cfd4fe7373a6f9392e
Mark <markingramtester@gmail.com>
2018-01-06 18:16:52 +0000

    Example code demonstrating formatting numbers

---
+++ Java_Book/Formats.java
@@ -0,0 +1,26 @@
+import java.text.NumberFormat ;
+import java.time.format.DateTimeFormatter ;
+
+
+public class Formats
+{
+	public static void main(String[] args) //Example code demonstrating formatting numbers
+	{		
+		NumberFormat nf = NumberFormat.getNumberInstance();
+		System.out.println( "\nNumber : " + nf.format(123456789) ); //Number with group separator
+		
+		NumberFormat cf = NumberFormat.getCurrencyInstance();
+		System.out.println( "\nCurrency : " + cf.format(1234.50f) ); //Number with currency
+
+		NumberFormat pf = NumberFormat.getPercentInstance();
+		System.out.println( "\nPercent : " +  pf.format(0.75f) ); //Number as %
+
+		java.time.LocalDateTime now = java.time.LocalDateTime.now(); //Gets current time
+
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("MMM d, yyy") ;
+		System.out.println( "\nDate : " + now.format( df ) ); //Current Date
+
+		DateTimeFormatter tf = DateTimeFormatter.ofPattern("h:m a") ;
+		System.out.println( "\nTime : " + now.format( tf ) ); //Current time
+	}
+}
